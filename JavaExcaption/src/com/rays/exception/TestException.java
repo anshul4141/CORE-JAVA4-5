18955e3e23c91b5ed78db0f636a8662c8f131d46
anshul4141 <anshulprajapati691@gmail.com>
2024-06-24 13:26:19 +0530

    done

---
+++ JavaExcaption/src/com/rays/exception/TestException.java
@@ -0,0 +1,47 @@
+package com.rays.exception;
+
+public class TestException {
+
+	public static void main(String[] args) {
+
+		// Example of ArithmeticException..
+		int a = 5;
+
+		try {
+			System.out.println(a / 0);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		System.out.println("++++++++++++++");
+
+		// Examole of NullPointerException..
+
+		String str = null;
+
+		try {
+
+			System.out.println(str.length());
+
+		} catch (NullPointerException e) {
+
+			System.out.println(e.getMessage() + " NullPointerException");
+
+		}
+
+		System.out.println("++++++++++++++");
+
+		// Examole of indexOutOfBond..
+		String[] str1 = { "a", "b", "c" };
+
+		try {
+
+			System.out.println(str1[3]);
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
